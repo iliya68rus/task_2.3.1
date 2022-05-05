6ea2db0ebefe184eaefeec2d9cb29a87152f1ac9
iliya68rus <iliya97pro@yandex.ru>
2022-05-05 19:57:35 +0300

    v 6

--- src/main/java/web/dao/UserDAOHibernateImpl.java
+++ src/main/java/web/dao/UserDAOHibernateImpl.java
@@ -1,12 +1,16 @@
 package web.dao;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import web.model.User;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@Transactional
 public class UserDAOHibernateImpl implements UserDAO {
     private static long ID_NUMBER;
     private List<User> userList;
@@ -18,42 +22,50 @@ public class UserDAOHibernateImpl implements UserDAO {
         userList.add(new User(++ID_NUMBER, "Aleks", "Evdokim", (byte) 46));
     }
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     public List<User> getAllUser() {
-        return userList;
+        return entityManager.createQuery("select u from User u", User.class).getResultList();
+       // return userList;
     }
 
     @Override
     public User getUserById(long id) {
-        for (User user : userList) {
-            if (user.getId() == id) {
-                return user;
-            }
-        }
-        return null;
+        return entityManager.find(User.class,id);
+//        for (User user : userList) {
+//            if (user.getId() == id) {
+//                return user;
+//            }
+//        }
+//        return null;
     }
 
     @Override
     public void saveUser(User user) {
-        userList.add(user);
-        user.setId(++ID_NUMBER);
+        entityManager.persist(user);
+//        userList.add(user);
+//        user.setId(++ID_NUMBER);
     }
 
     public void editUser(long id, User user) {
-        User userOld = getUserById(id);
-        userOld.setName(user.getName());
-        userOld.setLastName(user.getLastName());
-        userOld.setAge(user.getAge());
+        entityManager.merge(user);
+//        User userOld = getUserById(id);
+//        userOld.setName(user.getName());
+//        userOld.setLastName(user.getLastName());
+//        userOld.setAge(user.getAge());
     }
 
     @Override
     public void deleteUser(long id) {
-        int i = -1;
-        for (User user : userList) {
-            if (user.getId() == id) {
-                i = userList.indexOf(user);
-            }
-        }
-        userList.remove(i);
+        entityManager.remove(getUserById(id));
+//        int i = -1;
+//        for (User user : userList) {
+//            if (user.getId() == id) {
+//                i = userList.indexOf(user);
+//            }
+//        }
+//        userList.remove(i);
     }
 
 }
--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -14,7 +14,7 @@ public class User{
     @Column
     private String name;
 
-    @Column
+    @Column(name = "last_name")
     private String lastName;
 
     @Column
